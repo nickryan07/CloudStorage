392028fdbc5462934392970556f1a0b23d9bbd50
nickryan07 <nickryan07@sbcglobal.net>
2018-03-15 22:25:06 -0500

    Uploaded Correct Servlet
    Updated servlet

--- FileServlet/src/FileServlet.java
+++ FileServlet/src/FileServlet.java
@@ -4,21 +4,25 @@ import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
 
 /**
  * Servlet implementation class FileServlet
  */
 public class FileServlet extends HttpServlet {
-	//private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -29,22 +33,25 @@ public class FileServlet extends HttpServlet {
             ServletFileUpload upload = new ServletFileUpload(factory);
 
             try {
-                List items = upload.parseRequest(request);
-                Iterator iterator = items.iterator();
+                @SuppressWarnings("rawtypes")
+				List items = upload.parseRequest(request);
+                @SuppressWarnings("rawtypes")
+				Iterator iterator = items.iterator();
                 while (iterator.hasNext()) {
                     FileItem item = (FileItem) iterator.next();
 
                     if (!item.isFormField()) {
                         String fileName = item.getName();
 
-                        String root = getServletContext().getRealPath("/");
+                        String root = getServletContext().getRealPath("/uploads");
                         File path = new File(root);
                         if (!path.exists()) {
-                            boolean status = path.mkdirs();
+                            @SuppressWarnings("unused")
+							boolean status = path.mkdirs();
                         }
 
                         File uploadedFile = new File(path + "/" + fileName);
-                        System.out.println(uploadedFile.getAbsolutePath());
+                        //System.out.println(uploadedFile.getAbsolutePath());
                         item.write(uploadedFile);
                     }
                 }
@@ -55,5 +62,32 @@ public class FileServlet extends HttpServlet {
             }
         }
     }
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String fileName = request.getParameter("fileName");
+		if(fileName == null || fileName.equals("")){
+			throw new ServletException("File Name empty");
+		}
+		File file = new File(getServletContext().getRealPath("/uploads")+File.separator+fileName);
+		if(!file.exists()){
+			throw new ServletException("File doesn't exist");
+		}
+		System.out.println("File location on server::"+file.getAbsolutePath());
+		ServletContext ctx = getServletContext();
+		InputStream fis = new FileInputStream(file);
+		String mimeType = ctx.getMimeType(file.getAbsolutePath());
+		response.setContentType(mimeType != null? mimeType:"application/octet-stream");
+		response.setContentLength((int) file.length());
+		response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+		
+		ServletOutputStream os       = response.getOutputStream();
+		byte[] bufferData = new byte[1024];
+		int read=0;
+		while((read = fis.read(bufferData))!= -1){
+			os.write(bufferData, 0, read);
+		}
+		os.flush();
+		os.close();
+		fis.close();
+		System.out.println("File downloaded at client successfully");
+	}
 }
---
+++ FileServlet/target/run.bat
@@ -0,0 +1,3 @@
+@echo off
+java -jar FileServlet-0.0.1-SNAPSHOT.jar
+pause
